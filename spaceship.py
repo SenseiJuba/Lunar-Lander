7da442d67abe1eaa9000415ffbee567400a1b60a
Anzo <enzotmluigi.n64@gmail.com>
2021-07-21 14:36:28 +0200

    added spaceship.py

---
+++ spaceship.py
@@ -0,0 +1,14 @@
+##Spaceship class##
+import pygame
+from pygame import locals as const
+pg = pygame
+
+class Spaceship(pg.sprite.Sprite):
+    images = {
+        "idle" : None
+        }
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self) #pygame sprit init
+        self.image = self.images["idle"]
+        self.rect = self.image.get_rect()
+        self.angle = 0
